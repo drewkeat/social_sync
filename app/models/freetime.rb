27bb4eb9c8bf59dde5bafeb9b79dbbd5df36e922
Drew Keat <thedrewkeat@gmail.com>
2021-07-09 11:54:31 -0600

    create methods within Freetime model to display label, determine duration, determine whether two freetimes are compatible, find_common_time between two freetime objects

--- app/models/freetime.rb
+++ app/models/freetime.rb
@@ -1,3 +1,30 @@
 class Freetime < ActiveRecord::Base
     belongs_to :user
+
+    def duration
+        (self.end - self.start)/3600
+    end
+
+    def label
+        return "#{self.start.strftime("%B %d, %I:%M %p")} - #{self.end.strftime("%I:%M %p")}"
+    end
+
+    # Determines whether freetime can be aligned with self
+    def match?(freetime)
+        freetime.start.between?(self.start, self.end) || freetime.end.between?(self.start, self.end)
+    end
+
+    #Returns a new (unpersisted) Freetime object with matching availability if possible, else returns nil.
+    def find_common_time(freetime)
+        if freetime.start.between?(self.start, self.end) && freetime.end.between?(self.start, self.end)
+            return Freetime.new(start: freetime.start, end: freetime.end)
+        elsif freetime.end.between?(self.start, self.end)
+            return Freetime.new(start: self.start, end: freetime.end)
+        elsif freetime.start.between?(self.start, self.end)
+            return Freetime.new(start: freetime.start, end: self.end)
+        end
+        nil
+    end
+
 end
+
