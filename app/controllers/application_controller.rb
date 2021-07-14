f0bec8f33a254cfb31446556fb3784522094c586
Drew Keat <thedrewkeat@gmail.com>
2021-07-14 10:47:22 -0600

    secure session secret

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < Sinatra::Base
     register Sinatra::Flash
     set :public_folder, 'public'
     set :views, 'app/views'
-    set :session_secret, "secret" #?? Fix This
+    set :session_secret, ENV['SESSION_SECRET']
   end
 
   get "/" do
