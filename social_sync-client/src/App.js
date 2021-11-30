c8ffc3b4c6ce41ff0af7a9c1c15dabd84ef2df38
Drew Keat <thedrewkeat@gmail.com>
2021-11-29 23:41:01 -0700

    experiment with NavBar material component, and add temp routes

--- social_sync-client/src/App.js
+++ social_sync-client/src/App.js
@@ -1,9 +1,21 @@
+import NavBar from '../src/components/NavBar'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom'
+import Root from './pages/Root'
+import Profile from './pages/Profile'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element = {<Root />} />
+        <Route path="profile/*" element = {<Profile />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ social_sync-client/src/pages/Root.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+
+function Root() {
+  return (
+    <React.Fragment>
+      <div>
+        Landing Page
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default Root
